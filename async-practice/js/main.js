4ba0c631e3655bd5602e8a7d1f4668151b70ce07
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-14 00:04:41 +0000

    New commit

--- async-practice/js/main.js
+++ async-practice/js/main.js
@@ -1,16 +1,17 @@
-//Code 01
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
-// }
-// function houseTwo(){
-//     console.log('Paper delivered to house 2')
-// }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
-// }
-// houseOne()
-// houseTwo()
-// houseThree()
+// Code 01
+
+function houseOne(){
+    console.log('Paper delivered to house 1')
+}
+function houseTwo(){
+    console.log('Paper delivered to house 2')
+}
+function houseThree(){
+    console.log('Paper delivered to house 3')
+}
+houseOne()
+houseTwo()
+houseThree()
 
 //Code 02
 // function houseOne(){
