b601108e415a2f5dd3a848d4bb9d3d6833c98f6e
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-15 08:50:16 +0000

    new commit

--- async-practice/js/main.js
+++ async-practice/js/main.js
@@ -59,20 +59,20 @@
 // houseOne()
 
 //Code 05 Using Promise Implementation
-const promise = new Promise((resolve, reject) => {
-    const error = true
-    if(!error){
-        resolve('Promise has been fullfilled')
-    }
-    else
-    {
-        reject('Error: Operation has failed')
-    }
-})
-console.log(promise)
-promise
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+// const promise = new Promise((resolve, reject) => {
+//     const error = true
+//     if(!error){
+//         resolve('Promise has been fullfilled')
+//     }
+//     else
+//     {
+//         reject('Error: Operation has failed')
+//     }
+// })
+// console.log(promise)
+// promise
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
 
 //Code 06
 // function houseOne(){
@@ -105,44 +105,46 @@ promise
 //     .catch(err => console.log(err))
 
 //Code 07
-// function houseOne(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
-//     })
-// }
-// function houseTwo(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
-// }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
-//         }, 2000)
-//     })
+function houseOne(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 1')
+        }, 1000)
+    })
+}
+function houseTwo(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 2')
+        }, 5000)
+    })
+}
+function houseThree(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 3')
+        }, 2000)
+    })
+}
+
+// async function getPaid() {
+//   const houseOneWait = await houseOne();
+//   const houseTwoWait = await houseTwo();
+//   const houseThreeWait = await houseThree();
+//   console.log(houseOneWait);
+//   console.log(houseTwoWait);
+//   console.log(houseThreeWait);
 // }
 
-// async function getPaid(){
-//     const houseOneWait = await houseOne()
-//     const houseTwoWait = await houseTwo()
-//     const houseThreeWait = await houseThree()
-//     console.log(houseOneWait)
-//     console.log(houseTwoWait)
-//     console.log(houseThreeWait)
-// }
-
-// getPaid()
+// getPaid();
 
 //Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
+async function getACuteDogPhoto(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    // const err = await reject('Error')
+    console.log(data)
+    console.log(err);
 
+}
+getACuteDogPhoto()
